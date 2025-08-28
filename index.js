7c1e011114493ccf225d2c2c36b1d63db1d93982
ketan2035 <ketankumar147856@gmail.com>
2025-08-28 19:52:13 +0530

    new note is updated at the database

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose");
 const path=require("path");
 
 const app=express();
-const chat=require("./models/note");
+const note=require("./models/note");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 
@@ -19,12 +19,44 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/notepad');
 }
 
-app.get("/note",(req,res)=>{
-    res.send("working");
+let note1=new note({
+    title:"ketan",
+    content:"hhgfhjjgjkhhhjk",
+    written_at:new Date(),
+});
+
+// note1.save().then((res)=>{
+//     console.log(res);
+// })
+
+app.post("/new",(req,res)=>{
+    let {title ,content}=req.body;
+    let newnote=new note({
+        title:title,
+        content:content,
+        written_at:new Date(),
+    })
+    newnote
+        .save()
+        .then((res)=>{
+            console.log(res);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    res.redirect("/");
 })
 
-app.get("/",(req,res)=>{
-    res.send("working");
+app.get("/new", async(req,res)=>{
+    let notes= await note.find();
+    console.log("chats");
+    res.render("index.ejs",{notes});
+})
+
+app.get("/",async(req,res)=>{
+    let notes= await note.find();
+    console.log("chats");
+    res.render("index.ejs",{notes});
 })
 
 app.listen(8080,(req,res)=>{
--- models/note.js
+++ models/note.js
@@ -1,7 +1,17 @@
 const mongoose=require("mongoose");
 
 const noteSchema=new mongoose.Schema({
-    
+    title:{
+        type:String,
+        required:true
+    },
+    content:{
+        type:String,
+    },
+    written_at:{
+        type:Date,
+        required:true
+    }
 })
 
 const note=mongoose.model("note",noteSchema);
